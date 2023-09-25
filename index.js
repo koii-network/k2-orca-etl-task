6543c0beee566032bcd3fe777a3b86a21d158f40
Abhishek Chopra <abhishek@techvzero.com>
2023-09-25 20:43:27 +0530

    updated to using koii-x-orca

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
-const { OrcaPulse } = require('orca-pulse');
-const fs = require('fs')
-const ss = require('socket.io-stream');
+const { coreLogic } = require('./coreLogic');
+const { app } = require('./init');
+const {
+  namespaceWrapper,
+  taskNodeAdministered,
+} = require('./namespaceWrapper');
 
-const orca = new OrcaPulse()
-const process = require('process')
 
-orca.initialize(
-   'docker.io/1703706/cryo:v1-slim ',
-   'taskid1234',
-   'wss://localhost:3003',
-).then(() => {
-   collect = orca.podCall('collect', {
-      headers: {
-         'Content-Type': 'application/json'
-      },
-      body: {
-         'datatype': 'blocks',
-         'start_block': '100000',
-         'end_block': '100005',
-         'rpc_url': "https://internal.ethereum.n.chaindeck.io/278e1b2ab91f2ca96f7b8761bf65b9b2"
-      },
-   }).then(collect => {
+/**
+ * setup
+ * @description sets up the task node, particularly the inter-process communication to start and stop the task
+ * @returns {void}
+ */
+async function setup() {
+  console.log('setup function called');
+  // Run default setup
+  await namespaceWrapper.defaultTaskSetup();
+  process.on('message', m => {
+    console.log('CHILD got message:', m);
+    if (m.functionCall == 'submitPayload') {
+      console.log('submitPayload called');
+      coreLogic.submitTask(m.roundNumber);
+    } else if (m.functionCall == 'auditPayload') {
+      console.log('auditPayload called');
+      coreLogic.auditTask(m.roundNumber);
+    } else if (m.functionCall == 'executeTask') {
+      console.log('executeTask called');
+      coreLogic.task();
+    } else if (m.functionCall == 'generateAndSubmitDistributionList') {
+      console.log('generateAndSubmitDistributionList called');
+      coreLogic.submitDistributionList(m.roundNumber);
+    } else if (m.functionCall == 'distributionListAudit') {
+      console.log('distributionListAudit called');
+      coreLogic.auditDistribution(m.roundNumber);
+    }
+  });
 
-      console.log("etl data", collect)
-   }).catch(err => {
+}
 
-      console.log("error is in collect podcall", err)
-   })
+if (taskNodeAdministered) {
+  setup();
+}
 
-   setTimeout(() =>
-      {
-         orca.close()
-         process.exit()
-      }, 5000)
-}).catch((error) => {
-console.log("inside catch error", error)
-})
+if (app) {
+  //  Write your Express Endpoints here.
+  //  For Example
+  //  app.post('/accept-cid', async (req, res) => {})
+
+  // Sample API that return your task state
+
+  app.get('/taskState', async (req, res) => {
+    const state = await namespaceWrapper.getTaskState();
+    console.log('TASK STATE', state);
+
+    res.status(200).json({ taskState: state });
+  });
+  app.use('/api/', require('./routes') );
+}
---
+++ model/data.js
@@ -0,0 +1,105 @@
+const { namespaceWrapper } = require('../namespaceWrapper');
+
+/**
+ * Data class
+ *
+ * @param {string} name - the name of the database
+ * @param {object} data - the initial data to be stored in the database
+ *
+ * @returns {Data} - a Data object
+ *
+ */
+
+class Data {
+  constructor(name, data) {
+    this.name = name;
+    this.data = data;
+    this.dbprefix = `${name} + ":"`;
+    this.fullList = [];
+    this.lastUpdate = Date.now();
+  }
+
+  /**
+   * initializeData
+   * @returns {void}
+   */
+  async initializeData() {
+    if (this.db) return;
+    const db = await namespaceWrapper.getDb();
+    this.db = db;
+  }
+
+  /**
+   * create
+   * @param {*} item
+   * @returns {void}
+   */
+  async create(item) {
+    try {
+      const existingItem = await this.getItem(item);
+      // console.log('get item', existingItem);
+
+      if (existingItem) {
+        if (
+          !existingItem[0].timestamp ||
+          (item.timestamp && item.timestamp > existingItem[0].timestamp)
+        ) {
+          // Remove the old item with the same ID
+          await this.db.remove({ id: item.id }, {});
+          // console.log('Old item removed');
+          this.db.compactDatafile();
+        } else {
+          console.log('New item has a lower or equal timestamp; ignoring');
+          return undefined;
+        }
+      }
+
+      await this.db.insert(item);
+      // console.log('Item inserted', item);
+    } catch (e) {
+      console.error(e);
+      return undefined;
+    }
+  }
+
+  /**
+   * getItem
+   * @param {*} item
+   * @returns
+   * @description gets an item from the database by ID (CID)
+   */
+  async getItem(item) {
+    // console.log('trying to retrieve with ID', item.id);
+    try {
+      const resp = await this.db.find({ id: item.id });
+      // console.log('resp is ', resp);
+      if (resp.length !== 0) {
+        return resp;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  /**
+   * getList
+   * @param {*} options
+   * @returns
+   * @description gets a list of items from the database by ID (CID)
+   * or by round
+   */
+  async getList(options) {
+    // doesn't support options or rounds yet?
+    let itemListRaw;
+    console.log('has round', options.round);
+    // itemListRaw = await this.db.find({ item: { $exists: true } });
+    itemListRaw = await this.db.find({ round: options.round });
+
+    return itemListRaw;
+  }
+}
+
+module.exports = Data;
