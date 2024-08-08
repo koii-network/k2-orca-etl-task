ee9cfe4826e4f2b6d7c718686c58f8d36fc41b52
Laura Abro <laura@koii.network>
2024-08-08 12:43:50 -0300

    add testing scripts

---
+++ test.webpack.config.js
@@ -0,0 +1,20 @@
+import Dotenv from "dotenv-webpack";
+
+export default {
+	entry: "./index.js",
+	target: "node",
+	// When uploading to arweave use the production mode
+	// mode:"production",
+	mode: "development",
+	devtool: "source-map",
+	optimization: {
+		usedExports: false, // <- no remove unused function
+	},
+	stats: {
+		moduleTrace: false,
+	},
+	node: {
+		__dirname: true,
+	},
+	plugins: [new Dotenv()],
+};
